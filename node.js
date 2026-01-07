645191e843e61581b92e7c13f791cb9b0758d390
futanarikh7 <ok.ru.zahid@gmail.com>
2026-01-07 01:07:08 -0800

    Add energy regeneration and daily bonus logic
    Implement energy regeneration and daily balance update.

--- node.js
+++ node.js
@@ -28,3 +28,18 @@ app.post("/api/tap",(req,res)=>{
 });
 
 app.listen(3000);
+setInterval(()=>{
+  for(const id in users){
+    if(users[id].energy < 100){
+      users[id].energy += 1;
+    }
+  }
+},60000); // 1 energy per minute
+
+if(Date.now() - user.lastDaily > 86400000){
+  user.balance += 500;
+  user.lastDaily = Date.now();
+}
+if(referrer && referrer !== user_id){
+  users[referrer].balance += 1000;
+}
